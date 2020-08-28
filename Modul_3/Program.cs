b064b81a6efeb024da7ee6c247ff82d4200150d2
Pavel Yakovlev <28228@bk.ru>
2020-08-28 09:29:46 +0300

    Разобрался как делать меню

--- Modul_3/Program.cs
+++ Modul_3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Modul_3
 {
@@ -10,10 +11,38 @@ namespace Modul_3
     {
         static void Main(string[] args)
         {
+            #region Основное меню. Переменные
 
+            string head_menu = "Основное меню";
+            string head_game = "Игра в цифры";
+            int dif_level;
+            string dif_level_txt = "Уровень сложности: ";
+            string level_txt = "Уровень сложности";
+            string var_dif_level = $"{dif_level_txt}" + $"{level_txt}";
 
-            Console.WriteLine("Выбирите режим игры");
+            // Заголовок (название) игры. Задаем синий цвет, выставляем курсор по центру, пишем название игры
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (head_game.Length / 2), 0);
+            Console.WriteLine(head_game);
 
+            // Далее сбрасываем на цвет по умолчанию, выставляем курсор слева с оступом на одну строку по Y, просим ввести никнейм игрока
+            Console.ResetColor();
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine("Введите ваш никнейм :");
+
+            string nick_p1 = Console.ReadLine();
+            //            string nick_p2 = Console.ReadLine();
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine($"Ваш никнейм  \n \"{nick_p1}\"");
+            Console.SetCursorPosition(Console.WindowWidth - var_dif_level.Length, 0);
+            Console.WriteLine($"{var_dif_level}");
+            #endregion
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (head_menu.Length / 2), 4);
+            Console.WriteLine(head_menu);
+
+            Console.ResetColor();
             Console.WriteLine("1. Уровень сложности.");
             Console.WriteLine("2. Один против ИИ.");
             Console.WriteLine("3. Два игрока.");
@@ -27,10 +56,12 @@ namespace Modul_3
 
             while (true)
             {
-                Console.WriteLine("Выберите режим игры");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n Выберите режим игры");
+                Console.ResetColor();
                 gameMenu = Int32.Parse(Console.ReadLine());
 
-                if (gameMenu >=1 && gameMenu <= 4)
+                if (gameMenu >= 1 && gameMenu <= 4)
                 {
                     break;
                 }
@@ -39,15 +70,35 @@ namespace Modul_3
                 {
                     Console.WriteLine("Введите число от 1 до 4");
                 }
-                
+
             }
 
             switch (gameMenu)
             {
                 case 1:
-                    Console.WriteLine("Игра 1");
-                    break;
+                    Console.WriteLine("Уровень сложности: ");
+                    Console.WriteLine("Выберите уровень сложности: \n 1 - Легко \n 2 - Сложно");
+                    dif_level = Int32.Parse(Console.ReadLine());
+                    while (true)
+                    {
+                        // Выводим в столбик следующие надписи и ждем далее ввода пользователя (иначе цикл будет бесконечным)
+                        Console.WriteLine("Выберите уровень сложности: \n 1 - Легко \n 2 - Сложно");
+                        
+                        dif_level = Int32.Parse(Console.ReadLine());
 
+                        if (dif_level >= 1 && dif_level <= 2)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Выберите уровень сложности 1 или 2");
+                        }
+                    }
+
+                    
+                    break;
+                    
                 case 2:
                     Console.WriteLine("Игра 2");
                     break;
@@ -74,50 +125,20 @@ namespace Modul_3
             // а также отображаем уровень сложности
             // Задаем переменную заголовка игры и меню, выставляем цвет, положение
 
-            string head_menu = "Основное меню";
-            string head_game = "Игра в цифры";
-            byte dif_level = 1;
-            string dif_level_txt = "Уровень сложности: ";
-            string level_txt = "Уровень сложности";
-            string var_dif_level = $"{dif_level_txt}" + $"{level_txt}";
 
-            //switch (dificult)
-            //{
-            //    case dif_level == 1:
 
-            //    default:
-            //        break;
-            //}
-            // Заголовок (название) игры. Задаем синий цвет, выставляем курсор по центру, пишем название игры
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.SetCursorPosition((Console.WindowWidth / 2) - (head_game.Length / 2), 0);
-            Console.WriteLine(head_game);
+            //   
 
-            // Далее сбрасываем на цвет по умолчанию, выставляем курсор слева с оступом на одну строку по Y, просим ввести никнейм игрока
-            Console.ResetColor();
-            Console.SetCursorPosition(0, 1);
-            Console.WriteLine("Введите ваш никнейм :");
+            //    Console.WriteLine("1. Уровень сложности." + $"{dif_level}");
+            //    Console.WriteLine("2. Один против ИИ.");
+            //    Console.WriteLine("3. Два игрока.");
+            //    Console.WriteLine("4. Правила игры.");
 
-            string nick_p1 = Console.ReadLine();
-            //            string nick_p2 = Console.ReadLine();
-            Console.SetCursorPosition(0, 1);
-            Console.WriteLine($"Ваш никнейм \n\"{nick_p1}\"");
-            Console.SetCursorPosition(Console.WindowWidth - var_dif_level.Length, 0);
-            Console.WriteLine($"{var_dif_level}");
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.SetCursorPosition((Console.WindowWidth / 2) - (head_menu.Length / 2), 4);
-            Console.WriteLine(head_menu);
-
-            Console.WriteLine("1. Уровень сложности." + $"{dif_level}");
-            Console.WriteLine("2. Один против ИИ.");
-            Console.WriteLine("3. Два игрока.");
-            Console.WriteLine("4. Правила игры.");
-
-            Console.ReadKey();
+            //    Console.ReadKey();
 
 
             #endregion
+            //}
         }
     }
 }
